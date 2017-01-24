81f9d32628347565ec32663cc8b0f8cb68387c4e
andrew.keenan <andrew.keenan@LIVINGSTON.ORG>
2017-01-24 16:57:06 -0500

    Hardware Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/MoveStraight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/MoveStraight.java
@@ -14,19 +14,8 @@ public class MoveStraight extends LinearOpMode {
     Hardware3415 balin = new Hardware3415();
     public void runOpMode(){
         balin.init(hardwareMap, true);
-        balin.navx_device = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get(Hardware3415.cdim),
-                Hardware3415.NAVX_DIM_I2C_PORT,
-                AHRS.DeviceDataType.kProcessedData,
-                Hardware3415.NAVX_DEVICE_UPDATE_RATE_HZ);
-        //Prevents Balin from running before callibration is complete
-        while (balin.navx_device.isCalibrating()) {
-            telemetry.addData("Ready?", "No");
-            telemetry.update();
-        }
         telemetry.addData("Ready?", "Yes");
         telemetry.update();
-        balin.navx_device.zeroYaw();
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         balin.moveStraight(12, true, this);
