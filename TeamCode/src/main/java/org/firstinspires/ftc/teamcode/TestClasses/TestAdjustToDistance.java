4d5d3d2852fd6dc95a963cb2074d6382adc7a750
sporky1414 <sporky1414@gmail.com>
2017-02-22 16:39:17 -0500

    Added sonar test classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/TestAdjustToDistance.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.TestClasses;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.DriversAndHardware.Hardware3415;
+
+/**
+ * Created by andrew.keenan on 2/22/2017.
+ */
+
+public class TestAdjustToDistance extends LinearOpMode {
+    Hardware3415 balin = new Hardware3415();
+    ModernRoboticsI2cRangeSensor sonar;
+    public void runOpMode() {
+        balin.init(hardwareMap, true);
+        sonar = (ModernRoboticsI2cRangeSensor) hardwareMap.get("sonar");
+        waitForStart();
+        adjustToDistance(12.0, .3);
+    }
+
+    public void adjustToDistance(double distance, double power) {
+        double currDistance = readSonar();
+        while (distance - 2 < currDistance && currDistance < distance + 2 && opModeIsActive() && !isStopRequested()) {
+            if(currDistance < distance) {
+                balin.setDrivePower(-power);
+                while(currDistance < distance && opModeIsActive() && !isStopRequested()) {
+                    currDistance = readSonar();
+                }
+            }
+            else if (currDistance > distance) {
+                balin.setDrivePower(power);
+                while(currDistance > distance && opModeIsActive() && !isStopRequested()) {
+                    currDistance = readSonar();
+                }
+            }
+            currDistance = readSonar();
+        }
+        balin.restAndSleep(this);
+    }
+
+    public double readSonar() {
+        double sonarData = sonar.cmUltrasonic();
+        double odsData = sonar.cmOptical();
+        if(sonarData > 6) {
+            return sonarData;
+        }
+        else {
+            return odsData;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/TestRangeSensor.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.TestClasses;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.DriversAndHardware.Hardware3415;
+
+/**
+ * Created by andrew.keenan on 2/22/2017.
+ */
+
+public class TestRangeSensor extends LinearOpMode {
+    Hardware3415 balin = new Hardware3415();
+    ModernRoboticsI2cRangeSensor sonar;
+    public void runOpMode() {
+        balin.init(hardwareMap, true);
+        sonar = (ModernRoboticsI2cRangeSensor) hardwareMap.get("sonar");
+        waitForStart();
+        while(opModeIsActive()) {
+            telemetry.addData("Ultrasonic Data", sonar.cmUltrasonic());
+            telemetry.addData("ODS Data", sonar.cmOptical());
+            telemetry.update();
+        }
+    }
+}
